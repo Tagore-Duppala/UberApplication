7e57fb805b23f9c5c582cfaeffe14a6681c94445
Tagore-Duppala <tagoreduppala@gmail.com>
2024-11-24 12:03:17 +0530

    Rename fie names

--- src/main/java/com/project/rideBookingApplication/RideBookingApplication.java
+++ src/main/java/com/project/rideBookingApplication/RideBookingApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UberApplication {
+public class RideBookingApplication {
 
 	public static void main(String[] args) {
 
-			SpringApplication.run(UberApplication.class, args);
+			SpringApplication.run(RideBookingApplication.class, args);
 	}
 
 }
